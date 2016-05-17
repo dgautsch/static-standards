7c1673f52ae44caab49f0296141850cbee081936
Dan Gautsch <dan.gautsch@thinkbrownstone.com>
2016-05-16 20:01:04 -0400

    Adding smooth scroll ability to the navigation.

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -1,4 +1,41 @@
-(function(){
-	// Placeholder until we add UI interactions.
-	console.log("Scripts loaded");
-})();
+var TBI = TBI || {}
+
+;(function (TBI) {
+  var smoothScroll = (function () {
+    // Animate smooth page scroll to anchor on same page
+    // <a href="#target"></a> ... <div id="target"></div>
+    var animateScroll = function () {
+      if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+        var target = $(this.hash)
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000)
+          return false
+        }
+      }
+    }
+
+    var bindEvents = function (identifier) {
+      var $trigger = $(identifier)
+      $trigger.on('click', animateScroll)
+    }
+
+    return {
+      // Exposed Functions
+      scroll: function (identifier) {
+        bindEvents(identifier)
+      }
+    }
+  })()
+
+  TBI.smoothScroll = smoothScroll
+})(TBI)
+
+$(document).on('ready', function () {
+  var $scrollIdentifier = $('.tbi-nav-menu li a')
+  $scrollIdentifier.each(function () {
+    TBI.smoothScroll.scroll(this)
+  })
+})
